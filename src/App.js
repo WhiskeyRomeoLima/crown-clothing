f12a1705f6a09c21abc3e5c0fbc4f857cf5899e7
WhiskeyRomeoLima <wrlucas67@gmail.com>
2022-07-05 14:36:53 -0500

    Refactored Catogories to Directory.  Updated styles accordingly.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component'
 
 const categories = [
   {
@@ -33,12 +33,8 @@ const categories = [
 
 const App = () => {
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+    <Directory categories={categories} />
+  )
 };
 
 export default App;
